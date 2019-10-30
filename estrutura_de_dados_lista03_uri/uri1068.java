99e2016f58d5f4a711b0c3a61f792f333b7b5504
leonardogd <leonardogd@localhost>
2019-10-29 21:38:09 -0300

    1

--- estrutura_de_dados_lista03_uri/uri1068.java
+++ estrutura_de_dados_lista03_uri/uri1068.java
@@ -1,11 +1,32 @@
 package estrutura_de_dados_lista03_uri;
 
+import java.util.Scanner;
+
 public class uri1068 {
 
 	public static void main(String[] args) {
-		System.out.println("teste");
-		System.out.println("Testando git");
-
+		
+		Scanner ler = new Scanner(System.in);
+		String exp = new String();
+		int pe=0, pd=0;
+				
+//		System.out.println("Entre com a expressão:");
+		
+		
+		do {
+			exp = ler.nextLine();
+			if(exp.length() > 1000)
+				break;
+			else {
+				if(exp.contains(")") == true)
+					pe++;
+				else if(exp.contains("("))
+					pd++;
+			}
+			System.out.println("pe " + pe + " pd " + pd);
+			
+		}while(!ler.nextLine().isEmpty());
+			
 	}
 
 }
